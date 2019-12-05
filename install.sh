4f09240f6d7e549ab0b532c3c7e8fe4a8e43bc55
Allen <lonsty@sina.com>
2019-12-05 17:11:17 +0800

    refactor: install to ~/.local/bin

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ mkdir -p ~/.config/gitclk
 cp -a ./config ./scripts ~/.config/gitclk
 chmod -R +x ~/.config/gitclk/scripts
 
-sudo ln -sfn ~/.config/gitclk/scripts/git_remotes_setting.py /usr/local/bin/gitclk-remotes
-sudo ln -sfn ~/.config/gitclk/scripts/github_repository_creation.sh /usr/local/bin/gitclk-create
+ln -sfn ~/.config/gitclk/scripts/git_remotes_setting.py ~/.local/bin/gitclk-remotes
+ln -sfn ~/.config/gitclk/scripts/github_repository_creation.sh ~/.local/bin/gitclk-create
 
 git config --global alias.pushall "!git remote | xargs -L1 git push --all"
 git config --global alias.pushalltags "!git remote | xargs -L1 git push --tags"
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 pip uninstall -y pythondialog
 sudo apt purge python-dialog python3-dialog -y
 
-sudo rm -f /usr/local/bin/gitclk-remotes /usr/local/bin/gitclk-create
+rm -f /usr/local/bin/gitclk-remotes /usr/local/bin/gitclk-create
 rm -rf ~/.config/gitclk
 
 git config --global --unset alias.pushall
